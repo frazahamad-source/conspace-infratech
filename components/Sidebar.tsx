d348f38939f5372dc6e79caef0d6531850e3a3cf
frazahamad-source <frazahamad@gmail.com>
2026-02-22 18:47:16 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import {
     LayoutDashboard,
-    Image,
+    Image as ImageIcon,
     Home,
     Settings,
     LogOut,
