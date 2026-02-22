e43bc9d4b6952cbffef09d6d358e12b2bd30c3b7
frazahamad-source <frazahamad@gmail.com>
2026-02-22 18:14:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const nextConfig = {
     typescript: { ignoreBuildErrors: true },
     eslint: { ignoreDuringBuilds: true },
     images: {
-        remotePatterns: [
-            { protocol: 'https', hostname: 'images.unsplash.com' },
-            { protocol: 'https', hostname: 'res.cloudinary.com' }
-        ],
+        remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }, { protocol: 'https', hostname: 'res.cloudinary.com' }],
     },
 };
 module.exports = nextConfig;
